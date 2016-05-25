6281ceca4e4250cfd8fa9c5da74d69c951f7eb40
Claus Colloseus <ccprog@gmx.de>
2016-05-25 21:33:32 +0200

    v0.7.3

--- plugin/js/crosswordsearch.js
+++ plugin/js/crosswordsearch.js
@@ -1,5 +1,5 @@
 /*
-crosswordsearch Wordpress plugin v0.7.2
+crosswordsearch Wordpress plugin v0.7.3
 Copyright Claus Colloseus 2014 for RadiJojo.de
 
 This program is free software: Redistribution and use, with or
